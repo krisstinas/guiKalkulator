7e5b6322a2fec485d69552109c72d36c27901e83
Kristina <kristina.smiljanic97@gmail.com>
2019-10-14 20:06:09 +0200

    kreiran gui

---
+++ src/gui/GuiSaberiOduzmi.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+import javax.swing.*;
+import java.awt.*;
+/**
+ *
+ * @author Korisnik
+ */
+public class GuiSaberiOduzmi {
+    
+    private JFrame frame;
+    private JPanel panel;
+    
+    private JLabel lblA;
+    private JLabel lblC;
+    private JLabel lblB;
+    
+    private JTextField txtA;
+    private JTextField txtC;
+    private JTextField txtB;
+    
+    private JButton btnSaberi;
+    private JButton btnOduzmi;
+    
+    public GuiSaberiOduzmi(){
+        gui();
+    }
+    
+    public void gui(){
+    
+        frame = new JFrame("Kalkulator"); //Inicijalizacija i dodeljivanje naziva prozoru
+        frame.setSize(300, 300);//Podešavamo veličinu prozora
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE ); //Kada pritinesmo X na prozoru on će izaći iz programa
+        frame.setLocationRelativeTo(null);
+        
+        panel= new JPanel(new GridBagLayout());
+        
+        GridBagConstraints c = new GridBagConstraints();
+        
+      
+        txtA = new JTextField(null, 5);
+        lblA = new JLabel("A");
+        
+        txtB = new JTextField(null, 5);
+        lblB = new JLabel("B");
+        
+        txtC = new JTextField(null, 5);
+        lblC = new JLabel("C");
+        
+        btnSaberi = new JButton("Saberi");
+        btnOduzmi = new JButton("Oduzmi");
+        
+        c.insets = new Insets(20, 10, 20, 10);
+        
+        c.gridx = 0;
+        c.gridy = 0;
+        panel.add(lblA, c);
+        
+        c.gridx = 1;
+        c.gridy = 0;
+        panel.add(txtA,c);
+        
+        c.gridx = 0;
+        c.gridy = 2;
+        panel.add(lblB,c);
+        
+        c.gridx = 1;
+        c.gridy = 2;
+        panel.add(txtB,c);
+        
+        c.gridx = 0;
+        c.gridy = 3;
+        panel.add(lblC,c);
+        
+        c.gridx = 1;
+        c.gridy = 3;
+        panel.add(txtC,c);
+        
+        c.gridx = 0;
+        c.gridy = 4;
+        panel.add(btnSaberi,c);
+        
+        c.gridx = 1;
+        c.gridy = 4;
+        panel.add(btnOduzmi,c);
+        
+        
+        frame.add(panel);
+        frame.setVisible(true );//Omogućavamo da se prozor prikaže
+
+    }
+
+    public static void main(String[] args) {
+        
+        new GuiSaberiOduzmi();
+        
+    }
+    
+}
